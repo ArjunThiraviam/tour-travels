5d5725afe606c6a44edbfc043fcf114465a1cc60
ArjunThiraviam <arjunthiraviam@gmail.com>
2019-08-04 21:16:36 +0530

    Created Homepage and destination base content, Gulp task

---
+++ gulp/tasks/styles.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvar = require('postcss-simple-vars'),
+nested = require('postcss-nested'),
+cssImport = require('postcss-import'),
+mixins = require('postcss-mixins');
+
+gulp.task('stylestask', function() {
+    return gulp.src('./app/assets/styles/styles.css')
+        .pipe(postcss([cssImport, mixins, nested, cssvar, autoprefixer]))
+        .on('error', function(errorInfo){
+            console.log(errorInfo.toString());
+            this.emit('end');
+        })
+        .pipe(gulp.dest('./app/temp/styles/'));
+
+});
---
+++ gulp/tasks/watch.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp'),
+watch = require('gulp-watch'),
+browserSync = require('browser-sync').create();
+
+
+gulp.task('watch', function(){
+    browserSync.init({
+        server: {
+            baseDir : "app"
+        }
+    });
+    watch('./app/*.html', function() {
+        browserSync.reload();
+    });
+    watch('./app/assets/scripts/**/*.js', gulp.series('reloadpage'));
+    watch('./app/assets/styles/**/*.css', gulp.series('stylestask', 'cssInject'));
+});
+
+gulp.task('cssInject', function(){
+    return gulp.src('./app/temp/styles/styles.css')
+    .pipe(browserSync.stream());
+});
+
+gulp.task('reloadpage', function(){
+    browserSync.reload();
+});
