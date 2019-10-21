689a5912caa3f89f3317a94ab2a003a8ba9f0352
ArjunThiraviam <arjunthiraviam@gmail.com>
2019-10-21 20:17:34 +0530

    local sight seeing

--- app/assets/scripts/slider.js
+++ app/assets/scripts/slider.js
@@ -4,7 +4,7 @@ setInterval( function() {
 //    $('.slide-3').hide(); 
    console.log("Hidden image " + count);
    count += 1;
-   console.log("Shon image " + count);
+   console.log("Shown image " + count);
    $('.slide-' + count).addClass('show');
    
    if(count  == 4) {
@@ -15,3 +15,21 @@ setInterval( function() {
 }, 5000);
 
 $('.slide-1').addClass('show');
+
+var herocount = 1; 
+
+setInterval( function() { 
+   $('.heroslide-' + herocount).removeClass('heroshow'); 
+   console.log("Hidden image " + herocount);
+   herocount += 1;
+   console.log("Shown image " + herocount);
+   $('.heroslide-' + herocount).addClass('heroshow');
+   
+   if(herocount  == 5) {
+    $('.heroslide-1').addClass('heroshow');
+    herocount = 1;
+    
+   }
+}, 5000);
+
+$('.heroslide-1').addClass('heroshow');
